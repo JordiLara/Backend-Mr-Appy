8c93ed32048d1917fce66493d0e88522c56de09d
JordiLara <jordilarareyes@gmail.com>
2024-12-20 17:55:18 +0100

    deletes not used data

--- src/start_data.js
+++ src/start_data.js
@@ -1,5 +1,5 @@
 import User from "./models/userModel.js";
-import Book from "./models/bookModel.js";
+
 
 const insertInitialUserData = async () => {
   const userData = [
@@ -41,13 +41,6 @@ const insertInitialUserData = async () => {
   // Para actualizar todas las filas: updateOnDuplicate: Object.keys(User.rawAttributes)
   await User.bulkCreate(userData, { ignoreDuplicates: true });
 
-  const bookData = [
-    { title: "TituloA", year: 1955 },
-    { title: "TituloB", year: 1988 },
-    { title: "TituloC", year: 1475, user_id: 2 },
-  ];
-  // Insertar datos con opción ignoreDuplicates
-  await Book.bulkCreate(bookData, { ignoreDuplicates: true });
 };
 
 export { insertInitialUserData };
