a3502c950dc26951cfc5c9cf5b03584e26680829
JordiLara <jordilarareyes@gmail.com>
2025-01-15 16:08:12 +0100

    add getteamreviews to reviews routes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,12 +1,13 @@
 // src/routes/userRoutes.js
 import { Router } from "express";
-import { createReview, getReviews } from "../controllers/reviewController.js";
+import { createReview, getReviews, GetTeamReviews } from "../controllers/reviewController.js";
 import { authenticateToken } from "../middlewares/authenticateToken.js";
 
 const router = Router();
 
 // Rutas para obtener y modificar los datos de los usuarios
 router.get("/", authenticateToken(["user"]), getReviews);
+router.get("/team", authenticateToken(["manager"]), getTeamReviews);
 router.post("/", authenticateToken(["user"]), createReview);
 
 export default router;
