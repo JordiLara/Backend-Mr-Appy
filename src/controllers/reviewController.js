7f60535af1d5f5bd2cf8c78d12f32bb5d72df247
JordiLara <jordilarareyes@gmail.com>
2025-01-15 16:22:33 +0100

    add getteamreviews API to review controller

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,28 +1,5 @@
 import Review from "../models/reviewModel.js";
-
-export const getReviews = async (req, res) => {
-  try {
-    const reviews = await Review.findAll({
-      where: {
-        id_user: req.user.id_user,
-      },
-      order: [["created_at", "DESC"]],
-    });
-
-    // Enviar una respuesta al cliente
-    res.status(200).json({
-      code: 1,
-      message: "Lista de reviews",
-      reviews: reviews,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      code: -100,
-      message: "An error occurred while obtaining the reviews",
-    });
-  }
-};
+import User from "../models/userModel.js";
 
 export const createReview = async (req, res) => {
   try {
@@ -52,6 +29,66 @@ export const createReview = async (req, res) => {
       code: -100,
       message: "Ha ocurrido un error al crear la review",
       error: error,
+    });
+  }
+};
+
+export const getReviews = async (req, res) => {
+  try {
+    const reviews = await Review.findAll({
+      where: {
+        id_user: req.user.id_user,
+      },
+      order: [["created_at", "DESC"]],
+    });
+
+    // Enviar una respuesta al cliente
+    res.status(200).json({
+      code: 1,
+      message: "Lista de reviews",
+      reviews: reviews,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      code: -100,
+      message: "An error occurred while obtaining the reviews",
+    });
+  }
+};
+
+export const getTeamReviews = async (req, res) => {
+  try {
+    const id_team = req.user.id_team;
+
+    if (!id_team) {
+      return res.status(400).json({
+        code: -1,
+        message: "No se encontró un equipo asociado al gerente",
+      });
+    }
+
+    const reviews = await Review.findAll({
+      where: { id_team },
+      include: [
+        {
+          model: User,
+          attributes: ["name", "surname"],
+        },
+      ],
+      order: [["created_at", "DESC"]],
+    });
+    res.status(200).json({
+      code: 1,
+      message: "Reseñas del equipo obtenidas correctamente",
+      reviews,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      code: -100,
+      message: "Ha ocurrido un error al obtener las reseñas del equipo",
+      error,
     });
   }
 };
