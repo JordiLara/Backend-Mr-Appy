39cbe607baeeecb48dca3f43a1beafcdfc5bc4f9
JordiLara <jordilarareyes@gmail.com>
2025-01-19 18:40:24 +0100

    statsRoutes created

---
+++ src/routes/statsRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getUserStats } from "../controllers/statsController.js";
+import { authenticateToken } from "../middlewares/authenticateToken.js";
+
+const router = Router();
+
+router.get("/stats", authenticateToken(["user"]), getUserStats);
+
+export default Router;
